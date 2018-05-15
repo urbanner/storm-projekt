1b3b00be0239795d345681f0381af433d1ddf91f
surbanski <s.urbanski.h@gmail.com>
2018-05-15 23:58:21 +0200

    Przenesione i zintegrowane z projektem klasy budujace ranking z Apache Storm

--- src/main/java/org/uam/surbanski/storm/HashtagRankingTopologyBuilder.java
+++ src/main/java/org/uam/surbanski/storm/HashtagRankingTopologyBuilder.java
@@ -4,7 +4,9 @@ import org.apache.storm.generated.StormTopology;
 import org.apache.storm.topology.TopologyBuilder;
 import org.apache.storm.tuple.Fields;
 import org.uam.surbanski.storm.bolt.HashtagExtractorBolt;
+import org.uam.surbanski.storm.bolt.IntermediateRankingsBolt;
 import org.uam.surbanski.storm.bolt.RollingCountBolt;
+import org.uam.surbanski.storm.bolt.TotalRankingsBolt;
 import org.uam.surbanski.storm.spout.TwitterSpout;
 
 public class HashtagRankingTopologyBuilder {
@@ -13,7 +15,12 @@ public class HashtagRankingTopologyBuilder {
 
         builder.setSpout("twitter-spout", new TwitterSpout());
         builder.setBolt("hashtag-extractor", new HashtagExtractorBolt()).shuffleGrouping("twitter-spout");
-        builder.setBolt("hashtag-counter", new RollingCountBolt()).fieldsGrouping("hashtag-extractor", new Fields("hashtag"));
+        builder.setBolt("hashtag-counter", new RollingCountBolt(60, 5)).fieldsGrouping("hashtag-extractor", new
+                Fields("hashtag"));
+        builder.setBolt("intermediate-ranker", new IntermediateRankingsBolt(10, 10)).fieldsGrouping
+                ("hashtag-counter",
+                        new Fields("hashtag"));
+        builder.setBolt("total-ranker", new TotalRankingsBolt(10, 10)).globalGrouping("intermediate-ranker");
 
         return builder.createTopology();
     }

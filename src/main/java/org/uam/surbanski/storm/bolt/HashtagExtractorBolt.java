059e76a87bb969833c03d4853fb099034b07dfa5
surbanski <s.urbanski.h@gmail.com>
2018-05-16 18:34:53 +0200

    Zmiany aby topologia wysylala adres zwiazany z kontem uzytkownika, a nie lokalizacje urzadzenia (zbyt duzo nulli)

--- src/main/java/org/uam/surbanski/storm/bolt/HashtagExtractorBolt.java
+++ src/main/java/org/uam/surbanski/storm/bolt/HashtagExtractorBolt.java
@@ -7,7 +7,6 @@ import org.apache.storm.topology.base.BaseBasicBolt;
 import org.apache.storm.tuple.Fields;
 import org.apache.storm.tuple.Tuple;
 import org.apache.storm.tuple.Values;
-import twitter4j.GeoLocation;
 import twitter4j.Status;
 
 public class HashtagExtractorBolt extends BaseBasicBolt {
@@ -16,12 +15,12 @@ public class HashtagExtractorBolt extends BaseBasicBolt {
         Status status = (Status) tuple.getValueByField("tweet");
 
         String[] words = status.getText().trim().split("\\s+");
-        GeoLocation coordinates = status.getGeoLocation();
+        String coordinates = status.getUser().getLocation();
 
         for (String word : words) {
             if (StringUtils.startsWith(word, "#")) {
-                if (coordinates != null) basicOutputCollector.emit(new Values(word, coordinates.toString()));
-                else basicOutputCollector.emit(new Values(word, "GeoLocation{nieudostepnione}"));
+                if (coordinates != null) basicOutputCollector.emit(new Values(word, coordinates));
+                else basicOutputCollector.emit(new Values(word, "nieudostepnione"));
             }
         }
     }
--- src/main/java/org/uam/surbanski/storm/bolt/RollingCountBolt.java
+++ src/main/java/org/uam/surbanski/storm/bolt/RollingCountBolt.java
@@ -59,7 +59,7 @@ public class RollingCountBolt extends BaseRichBolt {
     private static final long serialVersionUID = 5537727428628598519L;
     private static final Logger LOG = Logger.getLogger(RollingCountBolt.class);
     private static final int NUM_WINDOW_CHUNKS = 5;
-    private static final int DEFAULT_SLIDING_WINDOW_IN_SECONDS = NUM_WINDOW_CHUNKS * 30;
+    private static final int DEFAULT_SLIDING_WINDOW_IN_SECONDS = NUM_WINDOW_CHUNKS * 120;
     private static final int DEFAULT_EMIT_FREQUENCY_IN_SECONDS = DEFAULT_SLIDING_WINDOW_IN_SECONDS / NUM_WINDOW_CHUNKS;
     private static final String WINDOW_LENGTH_WARNING_TEMPLATE =
             "Actual window length is %d seconds when it should be %d seconds"
